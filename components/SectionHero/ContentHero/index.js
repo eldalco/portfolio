0c21279db99e312e208249854d8600f65cae3c4a
eldalco <elkincortes03@gmail.com>
2022-07-24 23:33:06 -0500

    Content Hero component Created

---
+++ components/SectionHero/ContentHero/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from "./styles.module.css"
+
+
+const ContentHero = () => {
+    return (
+        <div className={styles["container__content"]}>
+            <div className={styles["div__title"]}>
+                <h1>Elkin Alfonso</h1>
+            </div>
+            <div className={styles["div__paragraph"]}>
+                <p>Soy Ingeniero de sistemas con más de 2 años de experiencia en labores de desarrollo de software. Particularmente me desempeño con tecnologías de JavaScript de Back end y Front end: React js, Next js, Node js, Express js.</p>
+            </div>
+            <div>
+                <div className={styles["div__btn"]}>
+                    <a href="#">Contáctame</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContentHero
