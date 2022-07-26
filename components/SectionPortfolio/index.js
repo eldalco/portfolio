18ef137c4085b350358950b97cec973cf1dfacd5
eldalco <elkincortes03@gmail.com>
2022-07-26 10:01:59 -0500

    Component portfolio created

---
+++ components/SectionPortfolio/index.js
@@ -0,0 +1,14 @@
+import styles from "./styles.module.css"
+
+const SectionPortfolio = () => {
+  return (
+    <section className={styles["c-section"]}>
+      <div className={styles["div__title"]}>
+        <h2>Portfolio</h2>
+      </div>
+     
+    </section>
+  )
+}
+
+export default SectionPortfolio
