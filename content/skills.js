7b9d3d8b46ad7845ad3f161f6e085716cfa0159e
eldalco <elkincortes03@gmail.com>
2022-07-26 10:01:05 -0500

    Data skills in content

---
+++ content/skills.js
@@ -0,0 +1,52 @@
+const skills =[  
+  {
+    name: "React JS",
+    logo: "/images/react-logo.webp"
+  },
+  {
+    name: "Next JS",
+    logo: "/images/next-logo.webp"
+  },
+  {
+    name: "Node JS",
+    logo: "/images/nodejs-logo.webp"
+  },
+  {
+    name: "Express JS",
+    logo: "/images/Expressjs-logo.webp"
+  },
+  {
+    name: "HTML",
+    logo: "/images/html-logo.webp"
+  },
+  {
+    name: "CSS",
+    logo: "/images/css-logo.webp"
+  },
+  {
+    name: "JavaScript",
+    logo: "/images/JavaScript-logo.webp"
+  },
+  {
+    name: "Typescript",
+    logo: "/images/typescript-logo.webp"
+  },
+  {
+    name: "Bootstrap",
+    logo: "/images/bootstrap-logo.webp"
+  },
+  {
+    name: "Figma",
+    logo: "/images/figma-logo.webp"
+  },
+  {
+    name: "Postman",
+    logo: "/images/postman-logo.webp"
+  },
+  {
+    name: "Git",
+    logo: "/images/git-logo.webp"
+  },
+  
+]
+export default skills
