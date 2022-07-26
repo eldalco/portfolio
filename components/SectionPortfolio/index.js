6b9f80b29907424ed25d82c364b65ed88793d812
eldalco <elkincortes03@gmail.com>
2022-07-26 17:01:10 -0500

    Import component ArchivePortfolio in section

--- components/SectionPortfolio/index.js
+++ components/SectionPortfolio/index.js
@@ -1,12 +1,15 @@
 import styles from "./styles.module.css"
+import ArchivePortfolio from "./ArchivePortfolio"
 
 const SectionPortfolio = () => {
   return (
     <section className={styles["c-section"]}>
-      <div className={styles["div__title"]}>
-        <h2>Portfolio</h2>
+      <div className={styles["section__content"]}>
+        <div className={styles["div__title"]}>
+          <h2>Portfolio</h2>
+        </div>
+        <ArchivePortfolio />
       </div>
-     
     </section>
   )
 }
