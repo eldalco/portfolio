7f8ff319e7e125c1d3d92982ddda8004db6e92e3
eldalco <elkincortes03@gmail.com>
2022-07-26 20:06:17 -0500

    Wrapper for content in hero

--- components/SectionHero/index.js
+++ components/SectionHero/index.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import styles from "./styles.module.css"
-import Picture from '../Picture'
-import ContentHero from './ContentHero'
-
+import React from 'react';
+import styles from "./styles.module.css";
+import Picture from '../Picture';
+import ContentHero from './ContentHero';
+import SliderWords from '../SliderWords';
 const SectionHero = () => {
 return (
     <section className={styles["section__hero"]}>
-        <div className={styles["div__picture"]}>
-            <Picture />
+        <div className={styles["div__hero"]}>
+            <div className={styles["div__picture"]}>
+                <Picture />
+            </div>
+            <div className={styles["div__content"]}>
+                <ContentHero />
+            </div>
         </div>
-        <div className={styles["div__content"]}>
-            <ContentHero />
+        <div>
+            <SliderWords />
         </div>
     </section>
 )
