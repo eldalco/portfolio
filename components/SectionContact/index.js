42119a68b311309591435bb85f666af5a2b0426b
eldalco <elkincortes03@gmail.com>
2022-07-26 20:03:51 -0500

    Component SectionContact

---
+++ components/SectionContact/index.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css"
+import ListContact from "./ListContact"
+const SectionContact = () => {
+  return (
+    <footer className={styles["footer__section"]}>
+      <ListContact />
+    </footer>
+  )
+}
+
+export default SectionContact
