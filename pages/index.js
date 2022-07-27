48e05394d7983b26e3f38ddc61292e877b124e0e
eldalco <elkincortes03@gmail.com>
2022-07-26 20:07:57 -0500

    import component contact in index

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import styles from '../styles/Home.module.css'
 import SectionHero from '../components/SectionHero'
 import SectionSkills from '../components/SectionSkills'
 import SectionPortfolio from '../components/SectionPortfolio'
+import SectionContact from '../components/SectionContact'
 import Nav from '../components/Nav'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <SectionHero />
       <SectionSkills />
       <SectionPortfolio />
+      <SectionContact />
     </div>
   )
 }
