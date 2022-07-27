3935d22403255ed00a6af893288d16417d340126
eldalco <elkincortes03@gmail.com>
2022-07-26 20:03:02 -0500

    change size picture

--- components/Picture/index.js
+++ components/Picture/index.js
@@ -4,7 +4,7 @@ import styles from "./styles.module.css"
 const Picture = () => {
     return (
         <div className={styles["container__img"]}>
-            <img className={styles["img__picture"]} src="./images/foto-edit.webp" alt="" />
+            <img className={styles["img__picture"]} src="./images/foto-edit2.webp" alt="" />
         </div>
     )
 }
