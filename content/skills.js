814d49870173463e23891b5eefa323307f260333
eldalco <elkincortes03@gmail.com>
2022-07-28 10:10:25 -0500

    new ids inserted

--- content/skills.js
+++ content/skills.js
@@ -1,49 +1,61 @@
 const skills =[  
-  {
+  { 
+    id: 0,
     name: "React JS",
     logo: "/images/react-logo.webp"
   },
   {
+    id: 1,
     name: "Next JS",
     logo: "/images/next-logo.webp"
   },
   {
+    id: 2,
     name: "Node JS",
     logo: "/images/nodejs-logo.webp"
   },
   {
+    id: 3,
     name: "Express JS",
-    logo: "/images/Expressjs-logo.webp"
+    logo: "/images/express-logo.png"
   },
   {
+    id: 4,
     name: "HTML",
     logo: "/images/html-logo.webp"
   },
   {
+    id: 5,
     name: "CSS",
     logo: "/images/css-logo.webp"
   },
   {
+    id: 6,
     name: "JavaScript",
     logo: "/images/JavaScript-logo.webp"
   },
   {
+    id: 7,
     name: "Typescript",
     logo: "/images/typescript-logo.webp"
   },
   {
+    id: 8,
     name: "Bootstrap",
     logo: "/images/bootstrap-logo.webp"
   },
   {
+    id: 9,
     name: "Figma",
     logo: "/images/figma-logo.webp"
   },
   {
+    id: 10,
     name: "Postman",
     logo: "/images/postman-logo.webp"
   },
   {
+    id: 11,
     name: "Git",
     logo: "/images/git-logo.webp"
   },
