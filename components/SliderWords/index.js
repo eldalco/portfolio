6aae0568217cbd9fef8aafc7292d07020f79a7a9
eldalco <elkincortes03@gmail.com>
2022-07-28 10:15:42 -0500

    Responsive in Component sliderWords

--- components/SliderWords/index.js
+++ components/SliderWords/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { motion } from "framer-motion";
-
+import styles from "./styles.module.css"
 const SliderWords = () => {
   const [slider, setSlider] = useState (0);
   const words = ["Responsabilidad", "Eficiencia", "Trabajo en equipo", "Comunicación"];
@@ -11,24 +11,29 @@ const SliderWords = () => {
   useEffect(() => {
     setData({        
       word: words[slider],               
-      });
+    });
   }, [slider]);
 
   let sliderValue = words.length -1;
-  setTimeout(()=> {
-      slider >= sliderValue ? setSlider(slider = 0): setSlider(slider + 1);
-  },3000);
+
+  useEffect(() => {    
+    const timer = setTimeout(()=> {
+        slider >= sliderValue ? setSlider(slider = 0): setSlider(slider + 1);
+    },4000);
+    return () => clearTimeout(timer)
+  }, [slider]);
 
   return (
-    <div>
+    <div className={styles["div__word"]}>
       <motion.h2
+        key={slider}
         initial={{
           opacity: 0,          
-          transition: { duration: 1.5, ease: [0.17, 0.67, 0.83, 0.95] },
+          transition: { duration: 0.5, ease: [0.77, 0.17, 0.93, 0.95] },
         }}
         animate={{
           opacity: 1,          
-          transition: { duration: 1.5, ease: [0.17, 0.67, 0.83, 0.95] },
+          transition: { duration: 0.5, ease: [0.17, 0.67, 0.83, 0.95] },
         }}
         exit={{
           opacity: 0,          
