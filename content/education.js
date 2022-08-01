b8a87130770db3e17813311c6674b7e897e63e20
eldalco <elkincortes03@gmail.com>
2022-08-01 11:31:53 -0500

    Content education

---
+++ content/education.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    id: 0,
+    status: "Finalizado",
+    title: "UNAD",
+    description: "Ingeniero de sistemas",
+    image: "/images/unad-logo.webp"
+  },
+  {
+    id: 1,
+    status: "En curso",
+    title: "Platzi",
+    description: "Enfoque en cursos de desarrollo",
+    image: "/images/platzi-logo.webp"
+  }
+]
+export default data;
