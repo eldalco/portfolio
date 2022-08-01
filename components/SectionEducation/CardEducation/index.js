3217546deb80b824ad0e07736ea326a4eb8ebfdf
eldalco <elkincortes03@gmail.com>
2022-08-01 11:34:37 -0500

    Component CardEducation with styles

---
+++ components/SectionEducation/CardEducation/index.js
@@ -0,0 +1,22 @@
+import styles from "./styles.module.css"
+
+const CardEducation = ({status, title, description, image}) => {
+  return (
+    <article className={styles["card__education"]}>
+      <div className={styles["block__status"]}>
+        <span>{status}</span>
+      </div>
+      <div className={styles["block__content"]}>
+        <div className={styles["content__image"]}>
+          <img src={image} alt={title} />
+        </div>
+        <div className={styles["content__text"]}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default CardEducation
