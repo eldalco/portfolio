e23659d227e8c023f8dd5ce8102ad790008d9864
eldalco <elkincortes03@gmail.com>
2022-08-01 11:35:24 -0500

    Component GridEducation

---
+++ components/SectionEducation/GridEducation/index.js
@@ -0,0 +1,17 @@
+import styles from "./styles.module.css"
+import data from "../../../content/education"
+import CardEducation from "../CardEducation"
+
+const GriEducation = () => {
+  return (
+    <div className={styles["row__education"]}>
+      {data.map((item)=> (
+        <div key={item.id} className={styles["column__education"]}>
+          <CardEducation status={item.status} image={item.image} title={item.title} description={item.description}/>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default GriEducation
