a2a8ff7b3a5e8c23b29b5d66d257a0c469076cae
eldalco <elkincortes03@gmail.com>
2022-08-01 19:45:53 -0500

    Indentation adjustment

--- content/education.js
+++ content/education.js
@@ -4,14 +4,14 @@ const data = [
     status: "Finalizado",
     title: "UNAD",
     description: "Ingeniero de sistemas",
-    image: "/images/unad-logo.webp"
+    image: "/images/unad-logo.webp",
   },
   {
     id: 1,
     status: "En curso",
     title: "Platzi",
     description: "Enfoque en cursos de desarrollo",
-    image: "/images/platzi-logo.webp"
-  }
-]
+    image: "/images/platzi-logo.webp",
+  },
+];
 export default data;
--- content/practicePortfolio.js
+++ content/practicePortfolio.js
@@ -2,56 +2,62 @@ const practicePortfolio = [
   {
     id: 0,
     title: "Plataforma Audiocare",
-    description: "Plataforma privada para registro y gestión de datos de pacientes para cada proceso de consulta. Autorización de peticiones por token, cargue de archivos en lote, autenticación por rol, descarga de historias en archivo .pdf, uso de React hooks y custom hooks, diseño a medida.",
+    description:
+      "Plataforma privada para registro y gestión de datos de pacientes para cada proceso de consulta. Autorización de peticiones por token, cargue de archivos en lote, autenticación por rol, descarga de historias en archivo .pdf, uso de React hooks y custom hooks, diseño a medida.",
     tech: "React JS, Bootstrap, Axios, Styled Components",
     image: "/images/portfolio/audiocare-platform.webp",
-    access: false
+    access: false,
   },
   {
     id: 1,
     title: "Back office Morph",
-    description: "Aplicación privada para administración de datos de proyecto inmobiliario Morph Granada. Autenticación de usuario, autorización de peticiones por token (JWT), consumo de Rest API, actualización de datos con cargue de archivo .xlsx, descarga de reportes.",
+    description:
+      "Aplicación privada para administración de datos de proyecto inmobiliario Morph Granada. Autenticación de usuario, autorización de peticiones por token (JWT), consumo de Rest API, actualización de datos con cargue de archivo .xlsx, descarga de reportes.",
     tech: "Next JS, Material UI, Axios",
     image: "/images/portfolio/morph_backoffice.webp",
-    access: false
+    access: false,
   },
   {
     id: 2,
     title: "CRM Eyetrade",
-    description: "CRM privado. Permite gestión por tracking de leads obtenidos de diversas fuentes. Asignación de perfil comercial para seguimiento, monitorización de cifras y estados. Acceso basado en roles, autorización por token.",
+    description:
+      "CRM privado. Permite gestión por tracking de leads obtenidos de diversas fuentes. Asignación de perfil comercial para seguimiento, monitorización de cifras y estados. Acceso basado en roles, autorización por token.",
     tech: "React JS, Bootstrap, Material Design, Chart.js",
     image: "/images/portfolio/eyetrade-crm.webp",
-    access: false
+    access: false,
   },
   {
     id: 3,
     title: "Página web Innteria",
-    description: "Página informativa de Innteria. Diseño y funcionalidad a medida, sliders automáticos, diseño responsivo, React Hooks, framer motion.",
+    description:
+      "Página informativa de Innteria. Diseño y funcionalidad a medida, sliders automáticos, diseño responsivo, React Hooks, framer motion.",
     tech: "Next JS, CSS, Vercel",
     image: "/images/portfolio/innteria-page.webp",
     access: true,
     github: "https://github.com/eldalco/innteria-page",
-    url: "https://innteria-page.vercel.app/"
+    url: "https://innteria-page.vercel.app/",
   },
   {
     id: 4,
     title: "App de filtros para API",
-    description: "Proyecto práctico. Consumo de API, custom hooks para tratamiento de datos. Filtros, paginado, tipado fuerte, grid layout.",
+    description:
+      "Proyecto práctico. Consumo de API, custom hooks para tratamiento de datos. Filtros, paginado, tipado fuerte, grid layout.",
     tech: "React JS, Typescript, CSS, Axios, Custom hooks, Vercel",
-    image: "/images/portfolio/innteria-page.webp",
+    image: "/images/portfolio/practice-project.webp",
     access: true,
     github: "https://github.com/eldalco/react-type-axios",
-    url: "https://react-type-axios.vercel.app/"
+    url: "https://react-type-axios.vercel.app/",
   },
   {
     id: 5,
     title: "Portafolio",
-    description: "Página web informativa personal. Diseño a medida, grid layout, flexbox, diseño responsivo.",
+    description:
+      "Página web informativa personal. Diseño a medida, grid layout, flexbox, diseño responsivo.",
     tech: "Next JS, CSS, React hooks, Vercel",
-    image: "/images/portfolio/innteria-page.webp",
+    image: "/images/portfolio/portfolio-elkin.webp",
     access: true,
     github: "https://github.com/eldalco/portfolio",
-    url: "https://portfolio-henna-three-89.vercel.app/"
+    url: "https://portfolio-henna-three-89.vercel.app/",
   },
-]
+];
 export default practicePortfolio;
