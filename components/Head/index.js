cac63dbb6b5228bde8aaa02c82779ee130f82eb3
eldalco <elkincortes03@gmail.com>
2022-08-01 19:43:57 -0500

    Component Head created

---
+++ components/Head/index.js
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+const HeadComponent = () => {
+  return (
+    <Head>
+      <title>Portafolio | Elkin Alfonso</title>
+    </Head>
+  );
+};
+
+export default HeadComponent;
