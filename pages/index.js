45410651bf739ec395e243813db52b4b70522b1e
eldalco <elkincortes03@gmail.com>
2022-08-01 19:47:46 -0500

    Import component Head in index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
-import styles from '../styles/Home.module.css'
-import SectionHero from '../components/SectionHero'
-import SectionEducation from "../components/SectionEducation"
-import SectionSkills from '../components/SectionSkills'
-import SectionPortfolio from '../components/SectionPortfolio'
-import SectionContact from '../components/SectionContact'
-import Nav from '../components/Nav'
+import styles from "../styles/Home.module.css";
+import SectionHero from "../components/SectionHero";
+import SectionEducation from "../components/SectionEducation";
+import SectionSkills from "../components/SectionSkills";
+import SectionPortfolio from "../components/SectionPortfolio";
+import SectionContact from "../components/SectionContact";
+import Nav from "../components/Nav";
+import Head from "../components/Head";
 
 export default function Home() {
   return (
     <div>
+      <Head />
       <Nav />
       <SectionHero />
       <SectionEducation />
@@ -16,5 +18,5 @@ export default function Home() {
       <SectionPortfolio />
       <SectionContact />
     </div>
-  )
+  );
 }
