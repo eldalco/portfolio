81290a3e5346f9861045b69118905664d07a9b64
eldalco <elkincortes03@gmail.com>
2022-08-10 09:46:31 -0500

    Update file cv

--- components/SectionHero/ContentHero/index.js
+++ components/SectionHero/ContentHero/index.js
@@ -24,7 +24,7 @@ const ContentHero = () => {
           </Link>
         </div>
         <div className={`${styles["div__btn-cv"]} ${styles["div__btn"]}`}>          
-          <a href="/files/CV_Elkin_Alfonso_Cortes.pdf" download="/files/CV_Elkin_Alfonso_Cortes.pdf">Descargar CV</a>          
+          <a href="/files/Elkin_Alfonso_CV-es.pdf" download="/files/CV_Elkin_Alfonso_Cortes.pdf">Descargar CV</a>          
         </div>
       </div>
     </div>
