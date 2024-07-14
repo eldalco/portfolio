03d49b9bf904f07548e1bde62b669e8db1ac7730
Elkin Alfonso <elkincortes03@gmail.com>
2024-07-13 19:43:07 -0500

    Update Head -add analytics

--- components/Head/index.js
+++ components/Head/index.js
@@ -4,6 +4,15 @@ const HeadComponent = () => {
   return (
     <Head>
       <title>Portafolio | Elkin Alfonso</title>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TR84CS0ZMC"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-TR84CS0ZMC');
+    </script>
     </Head>
   );
 };
